0d02265dcfbe04fc54110cc61108c6d0d857db0f
Juan <Juan@10.30.11.183>
2016-02-02 10:09:57 -0600

    edited methods#2#2

--- src/lab1/StartUp.java
+++ src/lab1/StartUp.java
@@ -15,15 +15,21 @@ public class StartUp {
         //this architecture can look a bit messy or not very professional
         //this architecture is very easy to understand
         
+        //Liskov's substitution principle can be beneficial if the class you 
+        //instatiate inherits and only contains the methods from the superclass.
+        //You might need to use a method that only belongs to the sub class 
+        
         IntroToProgrammingCourse c1 = new IntroToProgrammingCourse();
         
         c1.setPrerequisites("Hello");
         
         System.out.println(c1.getPrerequisites());
         
-        //Liskov's substitution principle can be beneficial if the class you 
-        //instatiate inherits and only contains the methods from the superclass.
-        //You might need to use a method that only belongs to the sub class 
+        ProgrammingCourse[] Courses = {new IntroToProgrammingCourse(), 
+         new IntroJavaCourse(), new AdvancedJavaCourse()};
         
-    }
+        for (ProgrammingCourse course: Courses){
+            System.out.println(course.getPrerequisites());
+        }
+    }    
 }
